72a1f8d4a9e4685bfeadde01647941cb152e7119
xutian <xutian0521@qq.com>
2019-02-26 11:25:54 +0800

    webpack basic usage

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,8 +1,20 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+
 function component() {
     var element= document.createElement('div');
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    // Lodash，现在由此脚本导入
     element.innerHTML= _.join(['Hello','webpack'],' ');
+    element.classList.add('hello');
+
+    // 将图像添加到我们现有的 div。
+    var myIcon=new Image();
+    myIcon.src=Icon;
+    element.appendChild(myIcon);
+    console.log(Data);
     return element;
 }
-
-document.body.appendChild(component());
+var div=component();
+document.body.appendChild(div);
