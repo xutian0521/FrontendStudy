11040932ccd79ee11a171bf4b81291bcef77b8b2
xutian <xutian0521@qq.com>
2019-03-02 11:16:38 +0800

    add vue-project-basic

---
+++ vue-course/vue-project-basic/src/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <img src="./images/logo.png" alt="logo" />
+        <span>{{msg}}</span>
+        <p class="test">test text</p>
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+    export default {
+        name:'app',
+        data(){
+            return {
+                msg:"hello vue !!"
+            }
+        },
+    }
+</script>
+<style >
+    .test{
+        color:#020202
+    }
+</style>
---
+++ vue-course/vue-project-basic/src/main.js
@@ -0,0 +1,12 @@
+
+import Vue from 'vue'
+import './styles/main.css'
+import App from './app.vue'
+
+new Vue({
+  el:"#app",
+  template:'<App/>',
+  components:{App}
+})
+
+console.log('hello vue!');
