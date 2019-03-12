32c195a27f71ff0d97783dda63c24f2a9e25766c
xutian <xutian0521@qq.com>
2019-03-12 14:56:52 +0800

    导出vue模板,两种写法

--- vue-course/vue-project-dev/src/module/app.vue
+++ vue-course/vue-project-dev/src/module/app.vue
@@ -2,19 +2,29 @@
     <div id="app">
         <img src="../images/logo.png"  alt="logo"/>
         <span>{{msg}}</span>
-        <p class="test" test text></p>
+        <p class="test"> test text</p>
     </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
-    name:'app2',
-    data(){
-        return {
-            msg:"hello vue~~##"
-        }
+/*--------------------写法一------------------------ */
+    export default {
+        name:'app',
+        data(){
+            return {
+                msg:"hello vue !!"
+            }
+        },
     }
-})
+/*--------------------写法二------------------------ */
+// export default Vue.extend({
+//     name:'app2',
+//     data(){
+//         return {
+//             msg:"hello vue~~##"
+//         }
+//     }
+// })
 </script>
 <style>
     .test{
