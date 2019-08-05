3838c9d37e348821d5cf833c8aaa17244395bd7e
xutian0521 <xutian0521@qq.com>
2019-08-05 17:36:31 +0800

    vue-basic 重命名为module

---
+++ vue-course/vue-module-basic/webpack.config.js
@@ -0,0 +1,38 @@
+const path =require('path');
+var webpack = require('webpack');
+// const HtmlWebpackPlugin= require('html-webpack-plugin');
+// const CleanWebpackPlugin =require('clean-webpack-plugin');
+const VueLoaderPlugin =require('vue-loader/lib/plugin')
+
+module.exports= {
+    entry:'./src/main.js',
+    output:{
+        filename:'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins:[
+      new VueLoaderPlugin(),
+        // new CleanWebpackPlugin(['dist']),
+        // new HtmlWebpackPlugin({
+        //     title: 'Output Management'
+        //   })
+
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.vue$/,
+            loader: ['vue-loader']
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+    resolve: {
+        alias: {
+          'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'
+        }
+      }
+}
