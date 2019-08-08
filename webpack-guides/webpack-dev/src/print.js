a6fca2cee206492089bc86e4997f58a8789d75ed
xutian0521 <xutian0521@qq.com>
2019-08-08 11:51:57 +0800

    wepack文件夹 重命名

---
+++ webpack-guides/webpack-dev/src/print.js
@@ -0,0 +1,4 @@
+export default function printMe() {
+    console.log(' o(*￣︶￣*)o');
+    //cosnole.error('I get called from print.js!');
+  }
---
+++ webpack-guides/webpack-simple/README.md
@@ -0,0 +1,4 @@
+## webpack 学习
+
+>官网入门指南：https://www.webpackjs.com/guides/installation/
+----
